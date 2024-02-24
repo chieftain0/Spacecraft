95f26f2937f15990f114fc540837a0dbeca3bf97
chieftain0 <bexruzerk3210@gmail.com>
2024-02-24 14:01:17 +0400

    initial commit

--- Assets/Scripts/SpacecraftController.cs
+++ Assets/Scripts/SpacecraftController.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SpacecraftController : MonoBehaviour
 {
-    public float thrust = 100;
+    public TMP_Text ControlModeUI;
+
+
+
+    public float thrust = 0.3f;
+    public float torque = 0.1f;
+
+    public bool ManualControllMode = false;
+
 
     public float LX;
     public float LY;
     public float RX;
     public float RY;
+
+    public float LB;
+    public float RB;
+    public float BUTTONS;
+
+    public float LT;
+    public float RT;
+    public float TRIGGERS;
 
     Rigidbody rb;
 
@@ -27,6 +45,58 @@ public class SpacecraftController : MonoBehaviour
         RX = Input.GetAxis("RX");
         RY = -Input.GetAxis("RY");
 
-        rb.AddForce(transform.up * thrust * LY);
+        if(Input.GetButton("LB"))
+        {
+            LB = -1f;
+        }
+        else
+        {
+            LB = 0f;
+        }
+        if(Input.GetButton("RB"))
+        {
+            RB = 1f;
+        }
+        else
+        {
+            RB = 0f;
+        }
+        BUTTONS = LB + RB;
+
+        LT = -Input.GetAxis("LT");
+        RT = Input.GetAxis("RT");
+
+        TRIGGERS = LT + RT;
+
+
+        if(Input.GetButtonDown("START"))
+        {
+            ManualControllMode = !ManualControllMode;
+        }
+        if (ManualControllMode)
+        {
+            ControlModeUI.text = "MANUAL control";
+        }
+        else
+        {
+            ControlModeUI.text = "ARCADE control";
+        }
+
+
+        if (ManualControllMode == true)
+        {
+            rb.AddRelativeForce(transform.right * thrust * LY);
+            rb.AddRelativeForce(-transform.forward * thrust * LX);
+            rb.AddRelativeForce(transform.up * thrust * TRIGGERS);
+
+            rb.AddRelativeTorque(-transform.forward * torque * RY);
+            rb.AddRelativeTorque(-transform.right * torque * RX);
+            rb.AddRelativeTorque(transform.up * torque * BUTTONS);
+        }
+        else
+        {
+
+        }
+
     }
 }
